493a3126a199f3366676e645d5a7601d644749b9
Caleb Crouch <cecrouch01@gmail.com>
2023-07-22 15:27:12 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            print "Hello World!";
+        ?>
+    </h1> 
+    <ul>
+        <li>
+            <?php 
+            echo "This is my first php website"
+            ?>
+        </li>  
+        <?php 
+        echo "<li> I am learning how to use echo at the moment </li>"
+        ?>
+</body>
+</html>
